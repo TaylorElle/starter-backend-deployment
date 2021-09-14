78b59001273088a5ff3397b92cdc21d7e12e8d4f
“Taylor <duggan.taylor24@yahoo.com>
2021-09-14 10:10:45 -0500

    update

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,10 @@
+// This line is required if you want to make use of the .env file.
 require("dotenv").config();
+
+//you'll see that a URL variable is set so that
+//if the NODE_ENV environment variable is equal to the string "production",
+//PRODUCTION_DATABASE_URL will be used.
+//Otherwise, DEVELOPMENT_DATABASE_URL will be used.
 const {
   NODE_ENV = "development",
   DEVELOPMENT_DATABASE_URL,
